6f05a85ced243f54d69f990ad7c07f7e80dddc06
BeYkeRYkt <beykerykt@gmail.com>
2017-03-24 11:29:17 +0700

    Update bdroid_buildcfg.h from teshi

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,4 +1,6 @@
-/*
+/**
+ * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2016 The CyanogenMod Project
  * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,14 +19,8 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "Wileyfox Swift 2"
+#define BTM_DEF_LOCAL_NAME   "Wileyfox Swift 2"
 
-#define MAX_ACL_CONNECTIONS    7
-#define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-#define BLE_PERIPHERAL_ADV_NAME  FALSE
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BTM_LE_SECURE_CONN  TRUE
 #define BT_CLEAN_TURN_ON_DISABLED 1
 #endif
