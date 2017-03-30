8551ee7646b6b06f0a344225f66517e64f66be3d
BeYkeRYkt <beykerykt@gmail.com>
2017-03-30 22:52:26 +0900

    marmite: rebase to marmite

---
+++ board/recovery.mk
@@ -0,0 +1,2 @@
+# Recovery
+TARGET_RECOVERY_FSTAB := device/wileyfox/marmite/rootdir/etc/fstab.qcom
--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,31 +11,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from marmite device
-$(call inherit-product, device/wileyfox/marmite/device.mk)
+$(call inherit-product, device/wileyfox/kipper/full_marmite.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Set those variables here to overwrite the inherited values.
+# Must define platform variant before including any common things
+TARGET_BOARD_PLATFORM_VARIANT := msm8937
+
+PRODUCT_NAME := lineage_marmite
 BOARD_VENDOR := wileyfox
 PRODUCT_DEVICE := marmite
-PRODUCT_NAME := lineage_marmite
-PRODUCT_BRAND := Wileyfox
-PRODUCT_MODEL := Swift 2
-PRODUCT_MANUFACTURER := Wileyfox
-
-# Overlays (inherit after vendor/cm to ensure we override it)
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_GMS_CLIENTID_BASE := android-wileyfox
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="Wileyfox/Swift2/marmite:6.0.1/MHC19Q/ZNH2KAS7EB:user/release-keys" \
-    PRIVATE_BUILD_DESC="Swift2-user 6.0.1 MHC19Q ZNH2KAS7EB release-keys"
+PRODUCT_MANUFACTURER := Wileyfox
+PRODUCT_MODEL := Wileyfox Swift 2
+
+PRODUCT_BRAND := Wileyfox
+TARGET_VENDOR := wileyfox
+TARGET_VENDOR_PRODUCT_NAME := Swift 2
+TARGET_VENDOR_DEVICE_NAME := marmite
